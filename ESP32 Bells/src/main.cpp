9e2dc4e195f16d79b992b927a15ad475cf79e10d
Amelia Meyer <agmlego@gmail.com>
2022-12-24 00:15:25 -0500

    Changed case because apparently this is not a constant...

--- ESP32 Bells/src/main.cpp
+++ ESP32 Bells/src/main.cpp
@@ -1,33 +1,33 @@
 #include <Arduino.h>
 #include "bells.h"
 
-uint8_t BELL;
+uint8_t bell;
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Setting up bells!");
-  for (BELL = 0; BELL < NUM_BELLS; ++BELL)
+  for (bell = 0; bell < NUM_bellS; ++bell)
   {
-    pinMode(bells[BELL], OUTPUT);
-    Serial.print(bell_notes[BELL]);
+    pinMode(bells[bell], OUTPUT);
+    Serial.print(bell_notes[bell]);
     Serial.print(" ");
   }
   Serial.println(" Done!");
-  BELL = 0;
+  bell = 0;
 }
 
 void loop()
 {
   Serial.print("Ringing ");
-  Serial.print(bell_notes[BELL]);
+  Serial.print(bell_notes[bell]);
   Serial.print("! ");
-  ringBell(BELL++);
+  ringbell(bell++);
   Serial.println("DONE");
   delayMicroseconds(1000 * 1000);
-  if (BELL >= NUM_BELLS)
+  if (bell >= NUM_bellS)
   {
     Serial.println("Resetting bells!");
-    BELL = 0;
+    bell = 0;
   }
 }
