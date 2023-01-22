4d98cacf2087d42f1865923e2fe1c09986897e8d
Jamie Bliss <jamie@ivyleav.es>
2023-01-21 19:41:02 -0500

    Working bells!!!

--- ESP32 Bells/src/bells.h
+++ ESP32 Bells/src/bells.h
@@ -5,12 +5,13 @@
 const uint8_t bells[] = {16, 17, 18, 19, 21, 22, 33, 32, 27, 26, 25, 23};
 const char *bell_notes[] = {"E7", "C7", "B6", "A#6", "A6", "G#6", "C6", "C#6", "D6", "B5/C6", "G#5", "F#6"};
 
-const uint32_t STRIKE_DURATION = 25 * 1000; // 25ms
+const uint32_t STRIKE_DURATION_MS = 25;
+const uint32_t STRIKE_DURATION_US = STRIKE_DURATION_MS * 1000;
 
 void ringBell(uint8_t bell)
 {
     digitalWrite(bells[bell], HIGH);
-    delayMicroseconds(STRIKE_DURATION);
+    delayMicroseconds(STRIKE_DURATION_US);
     digitalWrite(bells[bell], LOW);
 }
 
--- ESP32 Bells/src/main.cpp
+++ ESP32 Bells/src/main.cpp
@@ -7,10 +7,79 @@
 #include "bells.h"
 #include "ssid.h"
 
-unsigned long t0 = millis();
 int8_t isConnected = 0;
 
+const struct {
+  byte note;
+  uint8_t pin;  
+} bell_config[NUM_BELLS] = {
+  // E7
+  { 100, 16 },
+  // C7
+  { 96, 17 },
+  // B6
+  { 95, 18 },
+  // A#6
+  { 94, 19 },
+  // A6
+  { 93, 21 },
+  // G#6
+  { 92, 22 },
+  // C6
+  { 84, 33 },
+  // C#6
+  { 85, 32 },
+  // D6
+  { 86, 27 },
+  // B5
+  { 83, 26 },
+  // G#5
+  { 80, 25 },
+  // F#6
+  { 90, 23 }, // Suspect this is actually F#5/78
+};
+
+struct {
+  unsigned long stop_time;
+} bell_state[NUM_BELLS];
+
+const size_t NO_BELL = -1;
+
 APPLEMIDI_CREATE_DEFAULTSESSION_INSTANCE(); 
+
+size_t find_bell(byte midi_note) {
+  // TODO: Binary search?
+  for (size_t i = 0; i < NUM_BELLS; i++) {
+    if (bell_config[i].note == midi_note) {
+      return i;
+    }
+  }
+  return NO_BELL;
+}
+
+void printWifiData() 
+{
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+}
 
 void setup()
 {
@@ -20,6 +89,7 @@ void setup()
   for (uint8_t bell = 0; bell < NUM_BELLS; ++bell)
   {
     pinMode(bells[bell], OUTPUT);
+    bell_state[bell].stop_time = 0;
     Serial.print(bell_notes[bell]);
     Serial.print(" ");
   }
@@ -30,6 +100,8 @@ void setup()
     Serial.println("Waiting for WiFi connection");
     delay(1000);
   }
+
+  printWifiData();
 
   Serial.println("Beginning MIDI...");
   MIDI.begin(); // listens on channel 1
@@ -51,6 +123,12 @@ void setup()
     Serial.print(note);
     Serial.print(" ");
     Serial.println(velocity);
+
+    auto bellidx = find_bell(note);
+    if (bellidx != NO_BELL) {
+      digitalWrite(bell_config[bellidx].pin, HIGH);
+      bell_state[bellidx].stop_time = millis() + STRIKE_DURATION_MS;
+    }
   });
   MIDI.setHandleNoteOff([](byte channel, byte note, byte velocity) {
     Serial.print("NoteOff ");
@@ -71,8 +149,13 @@ void setup()
 void loop()
 {
   MIDI.read();
-  // printf("WiFi status: %i\n", WiFi.status());
-  // printWifiData();
-  // printCurrentNet();
-  // delay(10000);
+
+  auto now = millis();
+  for (size_t i = 0; i < NUM_BELLS; i++) {
+    auto stop_time = bell_state[i].stop_time;
+    if (stop_time != 0 && stop_time < now) {
+      digitalWrite(bell_config[i].pin, LOW);
+      bell_state[i].stop_time = 0;
+    }
+  }
 }
